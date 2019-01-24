445091ee7d2aa3fe9d5fef1dbd81ceea0c69b534
Omar Houmz <omar.houmz@schibsted.com>
2019-01-24 11:20:12 +0100

    * add reakit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Provider, Button } from "reakit"
+import theme from "reakit-theme-default"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Provider theme={theme}>
+    <Button>Button</Button>
+  </Provider>
+)
 
-export default App;
+export default App
